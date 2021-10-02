79247f3e8a2ede5512055336d06f811897da7e4f
ilyamikheev <mikheev.show@gmail.com>
2021-10-02 04:21:27 +0300

    fixed single handler annotation processor

--- telegram/src/main/kotlin/io/chengine/handler/TelegramSingleHandlerAnnotationProcessor.kt
+++ telegram/src/main/kotlin/io/chengine/handler/TelegramSingleHandlerAnnotationProcessor.kt
@@ -1,6 +1,7 @@
 package io.chengine.handler
 
 import io.chengine.handler.payment.TelegramHandlePreCheckout
+import io.chengine.handler.payment.TelegramHandleSuccessPayment
 import org.springframework.stereotype.Component
 import kotlin.reflect.KClass
 
@@ -14,6 +15,7 @@ class TelegramSingleHandlerAnnotationProcessor(handlerRegistry: DefaultHandlerRe
             TelegramHandlePollAnswer::class,
             TelegramHandleLocation::class,
             TelegramHandlePreCheckout::class,
+            TelegramHandleSuccessPayment::class,
             TelegramHandleContact::class,
             TelegramHandleMedia::class,
             TelegramHandleText::class,
